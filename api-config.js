c308f39d7d2582f9c19b4d978f132e0067f16a81
Shipnovashop <Adv.devendrasingh@zohomail.in>
2026-09-01 22:02:50 +0530

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,3 +1,2 @@
-// After deploying the backend on Render, replace the URL below with your Render service URL.
-// Example: window.SHIPNOVA_API = 'https://shipnovashop-api.onrender.com';
-window.SHIPNOVA_API = 'https://shipnovashop.onrender.com';
+window.SHIPNOVA_API =
+  "https://shipnovashop-1.onrender.com";
